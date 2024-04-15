4ea2d4839cd5d410d560b9a330e3f253b088f257
Akemi-Nakayama <akemi.nakayama.prestserv@petrobras.com.br>
2024-04-14 22:06:51 -0300

    Arrumando configuracao banco de dados aws rds

--- CadastroPratos/src/main/java/Projeto/CadastroPratos/model/entitie/CadastroPratoEntity.java
+++ CadastroPratos/src/main/java/Projeto/CadastroPratos/model/entitie/CadastroPratoEntity.java
@@ -3,6 +3,7 @@ package Projeto.CadastroPratos.model.entitie;
 import com.fasterxml.jackson.annotation.JsonProperty;
 
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
@@ -14,6 +15,7 @@ import javax.persistence.*;
 @NoArgsConstructor
 @Data
 @Table(name = "cadastro_prato")
+@Builder
 public class CadastroPratoEntity {
 
     @Id
